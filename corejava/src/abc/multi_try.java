38b65d5ade7e73700f4f59e37790f2bb829c716f
Pavankumargr <105409050+Pavankumargr@users.noreply.github.com>
2022-06-09 12:28:51 +0530

    3rd commit

---
+++ corejava/src/abc/multi_try.java
@@ -0,0 +1,35 @@
+package abc;
+
+public class multi_try {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stupublic class multi_try 
+        String st="welcome";
+		int x[]= {0,1,2,3};
+		try {
+			int slength=st.length();
+			System.out.println("string length:"+slength);
+		try {
+			int y=6;
+			System.out.println(x[y]);
+		}
+	
+		catch(ArithmeticException e)
+		{
+			System.out.println("exception is thrown");
+			System.out.println(e.toString());
+		}
+	}
+	catch(ArrayIndexOutOfBoundsException ae)	
+		
+	{
+	System.out.println("exception is thrown");
+	System.out.println(ae.toString());
+	}
+	System.out.println("i am other catch block");
+		}
+}
+
+	
+
+
